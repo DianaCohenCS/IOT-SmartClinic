a5971c3d7eaf0dcac4bdc5ec7f6fc23db1418202
Diana Cohen <diana.cohen@campus.technion.ac.il>
2024-10-21 16:44:13 +0300

    Add files via upload
    move basic definitions for FS here

---
+++ ESP32/fsDEFS.h
@@ -0,0 +1,3 @@
+#define FS_TYPE SPIFFS  // declare another type for testing, e.g. LittleFS
+//#define FS_TYPE LittleFS // ERROR: 'LittleFS' was not declared in this scope
+#define FS_FORMAT false   // You only need to format the filesystem once
